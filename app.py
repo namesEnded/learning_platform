96ea0450ce922fc86ced5ef64e604bccc3dde123
Arseniy Aphonin <ars.20013@yandex.ru>
2022-01-29 18:44:50 +0300

    add: success/error notifications, menu rendering dictionary

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, url_for, request, redirect
+import secrets
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///learningPlatform.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
 db = SQLAlchemy(app)
+
+menuItems = [{"name": "Main", "url": "/"},
+             {"name": "About", "url": "/about"},
+             {"name": "Create course", "url": "/create_course"},
+             {"name": "Courses", "url": "/courses"}]
 
 
 class Course(db.Model):
@@ -23,43 +30,49 @@ class Course(db.Model):
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", menuItems=menuItems)
 
 
 @app.route('/create_course', methods=['POST', 'GET'])
 def create_course():
+    # TODO: Make a normal request success handler
     if request.method == "POST":
         title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
-
         course = Course(title=title, intro=intro, text=text)
-
         try:
-            db.session.add(course)
-            db.session.commit()
-            return redirect('/courses')
+            if len(title) == 0:
+                flash('ERROR: error while sending data!', category='danger')
+                return render_template("create_course.html", menuItems=menuItems)
+            else:
+                db.session.add(course)
+                db.session.commit()
+                flash('Data sent successfully!', category='success')
+                return render_template("create_course.html", menuItems=menuItems)
+
         except:
-            return "ERROR: error while sending data"
+            flash('ERROR: error while sending data!', category='danger')
+            return
     else:
-        return render_template("create_course.html")
+        return render_template("create_course.html", menuItems=menuItems)
 
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", menuItems=menuItems)
 
 
 @app.route('/courses')
 def courses():
     all_courses = Course.query.order_by(Course.date.desc()).all()
-    return render_template("courses.html", all_courses=all_courses)
+    return render_template("courses.html", all_courses=all_courses, menuItems=menuItems)
 
 
 @app.route('/courses/<int:id>')
 def course_detail(id):
     course = Course.query.get(id)
-    return render_template("course_detail.html", course=course)
+    return render_template("course_detail.html", course=course, menuItems=menuItems)
 
 
 @app.route('/courses/<int:id>/delete')
@@ -90,8 +103,7 @@ def course_update(id):
         except:
             return "ERROR: error while updating data"
     else:
-        return render_template("course_update.html", course=course)
-
+        return render_template("course_update.html", course=course, menuItems=menuItems)
 
 
 if __name__ == '__main__':
